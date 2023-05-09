7c70a8d5e571d3219a317b8c75bb7dd11e3861bb
jjeg1979 <jjeg1979@gmail.com>
2023-05-09 08:08:08 +0200

    Parser class and first testing trial

---
+++ tests/test_btgenbox.py
@@ -0,0 +1,35 @@
+"""Tests the concretate implementarion of BtParser for Genbox-type Backtests"""
+
+# Standard library imports
+from pathlib import Path
+
+# Non-standard library imports
+import pytest
+
+# Project imports
+from ..src import BT_FILES
+from ..src.parser.btparser import (
+    BtPlatforms,
+    BtPeriods,
+    BtOrderType,
+)
+
+from ..src.parser.btgenbox import BtGenbox
+
+
+# Dummy test - Courtesy of JJ
+def test_btgenbox_import_works_properly():
+    assert True == True
+    
+
+@pytest.fixture()
+def bt():
+    return BtGenbox(Path(r'../parser/payload/'), BT_FILES[0])
+
+
+def test_btgengox_init_contructs_correctly(bt):
+    assert bt.path == Path(r'../parser/payload/')
+    assert bt.file == BT_FILES[0]
+    assert bt.platform == BtPlatforms.GBX
+    assert bt.period == BtPeriods.IS
+     
